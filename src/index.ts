ef0a666f24fbe58e46b44496f6bf14af95f5a468
Kianoosh Soleimani <mazyar.solo@gmail.com>
2024-02-21 16:10:18 +0330

    feat: export needed part of package

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./styles/index.css";
+export * from "./@types/_index";
+export * from "./components/_index";
+export * from "./context/ViewContextProvider";
+export * from "./hooks/_index";
+export * from "./utils/_index";
