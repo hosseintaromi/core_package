d3f9ee964a9752a5d1adf640f594a01153da6e19
katebmedia <katebmedia@gmail.com>
2024-05-07 12:49:38 +0330

    fix: :bug: fix slider style

--- src/stories/slider/slider.stories.tsx
+++ src/stories/slider/slider.stories.tsx
@@ -1,10 +1,10 @@
 import { useRef } from "react";
-import SlideContainer from "../../components/containers/SlideContainer";
 import { EventType, useInit } from "../../hooks";
 import MenuInlineSample from "./MenuInlineSample";
 import MenuInlineSample2 from "./MenuInlineSample2";
 import MenuInlineSample3 from "./MenuInlineSample3";
 import "./slider.css";
+import { SlideContainer } from "../../components";
 
 const SliderComponent = () => {
   const elRef = useRef<HTMLElement | undefined>();
@@ -13,23 +13,22 @@ const SliderComponent = () => {
 
   return (
     <>
-      <div style={{ overflowY: "scroll", height: "93vh" }}>
-        <div style={{ display: "flex" }}>
-          <SlideContainer
-            config={{
-              event: EventType.Tap,
-              components: [
-                { title: "All", component: MenuInlineSample },
-                { title: "Groups", component: MenuInlineSample2 },
-                { title: "Private Chats", component: MenuInlineSample3 },
-              ],
-              hasPointer: true,
-              firstIndex: 2,
-              elRef: elRef as any,
-              className: "slide-menu",
-            }}
-          />
-          {/* <OverlaySlideContainer
+      <div style={{ display: "flex" }}>
+        <SlideContainer
+          config={{
+            event: EventType.Tap,
+            components: [
+              { title: "All", component: MenuInlineSample },
+              { title: "Groups", component: MenuInlineSample2 },
+              { title: "Private Chats", component: MenuInlineSample3 },
+            ],
+            hasPointer: true,
+            firstIndex: 2,
+            elRef: elRef as any,
+            className: "slide-menu",
+          }}
+        />
+        {/* <OverlaySlideContainer
           config={{
             id: "setting1",
             event: EventType.Tap,
@@ -38,7 +37,6 @@ const SliderComponent = () => {
             className: "slide-menu",
           }}
         /> */}
-        </div>
       </div>
     </>
   );
